2d69b9382a72eea6e558af5b50ecce392193ffbc
Vlodkow <vlodkow@gmail.com>
2024-06-11 15:44:43 +0300

    Update relay bridge

--- modules/relay_bridge/module.py
+++ modules/relay_bridge/module.py
@@ -4,12 +4,12 @@ from helpers.factory import run_script
 
 
 def interface_relay_bridge():
-    available_chains = ['arbitrum', 'nova', 'ethereum', 'optimism', 'zksync', 'base', 'zora']
-    disabled_chains = [key for key in CHAINS.keys() if key not in available_chains]
+	available_chains = ['arbitrum', 'nova', 'ethereum', 'optimism', 'zksync', 'base', 'zora', 'scroll']
+	disabled_chains = [key for key in CHAINS.keys() if key not in available_chains]
 
-    from_network = print_input_network("From network", disabled_chains)
-    to_network = print_input_network("To network", disabled_chains)
-    amount_str = print_input_amounts_range()
+	from_network = print_input_network("From network", disabled_chains)
+	to_network = print_input_network("To network", disabled_chains)
+	amount_str = print_input_amounts_range()
 
-    params = [from_network, to_network]
-    run_script(relay_call, from_network, amount_str, params)
+	params = [from_network, to_network]
+	run_script(relay_call, from_network, amount_str, params)
